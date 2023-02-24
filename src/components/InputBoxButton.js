e229b2654a7c441e41adbf0f124f68180952883a
alison <tmrfandom@gmail.com>
2023-02-23 22:27:04 -0800

    Add github pages support. Add popover info box.

--- src/components/InputBoxButton.js
+++ src/components/InputBoxButton.js
@@ -5,19 +5,28 @@ import TextField from '@mui/material/TextField';
 import SendIcon from '@mui/icons-material/Send';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import Typography from '@mui/material/Typography';
+import Popover from '@mui/material/Popover';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 
-function InputBoxButton({answer, route, endAdornment="", startAdornment="", hint}){
+function InputBoxButton({answer, route, endAdornment="", startAdornment="", hint, image, imageTitle, location, description}){
 
     const [field, setField] = useState('');
     const [error, setError] = useState(false);
     const [errorText, setErrorText] = useState('');
     const [displayHint, setDisplayHint] = useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (answer === field) {
-            window.location.href = route
+            setAnchorEl(e.currentTarget);
         } else {
             setError(true);
             setErrorText('Incorrect.')
@@ -73,6 +82,57 @@ function InputBoxButton({answer, route, endAdornment="", startAdornment="", hint
                     onClick = {handleSubmit} >
                     submit
                      </Button>
+                        <Popover
+                            id={id}
+                            open={open}
+                            anchorEl={anchorEl}
+                            //onClose={handleClose}
+                            anchorOrigin={{
+                                vertical: 'center',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'center',
+                                horizontal: 'center',
+                            }}
+                        >
+                        <Card sx={{ width: 500 }}>
+                        <CardMedia
+                            sx={{ height: 350 }}
+                            image= {image}
+                            title= {imageTitle}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                            {location}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                            {description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                        <Grid item>
+                            <Grid 
+                            container
+                            spacing={5}
+                            direction="row"
+                            justifyContent="center"
+                            alignItems="center">
+                            <Grid item>
+                            <Button size="small"
+                                onClick = {() => {
+                                    window.location.href = "/";
+                                }}
+                            >Continue</Button>
+                            </Grid>
+                            <Grid item>
+                            <Button size="small">Open in Google Maps</Button>
+                            </Grid>
+                            </Grid>
+                        </Grid>
+                        </CardActions>
+                        </Card>
+                        </Popover>
                 </Grid>
                 <Grid item>
                     <Button variant="contained"
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import InputBoxButton from '../components/InputBoxButton';
 import InputAdornment from '@mui/material/InputAdornment';
 import Typography from '@mui/material/Typography';
+import Beach from '../images/Beach.png';
 
 function Start(){
 
@@ -37,8 +38,15 @@ function Start(){
                     </Typography>
 
                 </Grid>
-                <InputBoxButton answer = {'22.308'} route = {"/"} endAdornment = {<InputAdornment position="end">N°</InputAdornment>} hint = {'Destination Coordinate'}/>
-                
+                <InputBoxButton answer = {'22.308'}
+                                route = {"/"}
+                                endAdornment = {<InputAdornment position="end">N°</InputAdornment>} 
+                                hint = {'Destination Coordinate'}
+                                image = {Beach}
+                                imageTitle = {'Beach Slide'}
+                                location = {'HKG'}
+                                description = {'This project is a puzzle game that leads you on a tour through the places that matter the most, using the great public transportation I remember very well. While I was creating it, I discovered that anyone can contribute to Google Street View by posting their own 360° pictures, so those were very helpful in capturing places that aren’t a building or aren’t right next to a street. I didn’t even know the actual name of some of these places; I just used my knowledge of how to get there and found out the name on Google Maps. Having not been there since 2019, I was surprised I still remembered these things. '}
+                                 />
             </Grid>
         </Box>
        
